4da374ec15d59ece817d002dabf3d63c6c5344b3
Mateusz <m.zaniewski1995@gmail.com>
2024-01-21 13:30:45 +0100

    add desktop navigation

--- src/app/components/Header/HeaderDesktop.tsx
+++ src/app/components/Header/HeaderDesktop.tsx
@@ -1,5 +1,22 @@
+"use client"
+import styles from "../../styles/_headerDesktop.module.sass"
+import { Logo } from "../Logo/Logo";
+import { NavDesktop } from "../Nav/NavDekstop";
+import { CustomButton } from "../UI/Buttons/CustomButton";
+import {useScrollChecker} from "../../customHooks/scrollChecker";
 export const HeaderDesktop = ():JSX.Element => {
+    const pageY = useScrollChecker();
+    console.log(pageY)
+
     return (
-        <header></header>
+        <header className={`${styles.header__desktop} ${pageY > 0 ? styles.header__desktop__scrolled : ""}`}>
+            <div className={styles.header__desktop__nav}>
+                <div className={styles.header__desktop__nav__logo}>
+                    <Logo customStyles={{width:"auto", transform:"scale(0.7)", margin:"0", color: `${pageY > 0 ? "#000": "#fff"}`}}/>
+                </div>
+                <NavDesktop scrolled={pageY > 0 ? true : false}/>
+            </div>
+            <CustomButton path={'/contact'} text={"Napisz do mnie!"} color={pageY > 0 ? "black" : 'white'} icon={"mes"}/>
+        </header>
     )
 } 
--- src/app/components/Nav/NavDekstop.tsx
+++ src/app/components/Nav/NavDekstop.tsx
@@ -1,7 +1,14 @@
-export const NavMobile = ():JSX.Element => {
+import styles from "../../styles/_navDesktop.module.sass"
+export const NavDesktop:React.FC<{scrolled:boolean}> = (props):JSX.Element => {
     return (
-        <nav>
-            
+        <nav className={`${styles.desktop} ${ props.scrolled ? styles.desktop__scrolled: ""}`}>
+             <ul className={styles.desktop__list}>
+                <li className={styles.desktop__list__item}>Strona Główna</li>
+                <li className={styles.desktop__list__item}>O mnie</li>
+                <li className={styles.desktop__list__item}>Pakiety</li>
+                <li className={styles.desktop__list__item}>Opinie</li>
+                <li className={styles.desktop__list__item}>Kontakt</li>
+            </ul>
         </nav>
     )
 }
