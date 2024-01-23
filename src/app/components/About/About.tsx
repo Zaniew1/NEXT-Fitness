950f4c0f6940f4c3c7cc09df6fbf9e2311ffffc2
Mateusz <m.zaniewski1995@gmail.com>
2024-01-23 22:12:03 +0100

    create about componet

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,15 +1,17 @@
-import styles from "../../styles/_about.module.sass"
-import { CustomImage } from "../UI/Image/Image"
-export const About:React.FC = ():JSX.Element => {
+import classes from '../../styles/_about.module.sass';
+import { CustomButton } from '../UI/Buttons/CustomButton';
+import { CustomImage } from '../UI/Image/CustomImage';
+export const About:React.FC =():JSX.Element => {
     return(
-        <div className={styles.about}>
-            {/* <div>
-                <p></p>
-                <p></p>
-                <p></p>
-                <button></button>
-            </div> */}
-            <CustomImage src={'/plecy.jpg'} alt={'Plecy jakiejś Pani'} priority={true} quality={100} sizes={"100vw"}/>
-        </div>
+        <section className={classes.about}>
+              <div className={classes.about__image}>
+                    {/* <CustomImage customClass={classes.about__image__img} src={'/plecy.jpg'} alt={'asd'} priority={false} quality={60}/> */}
+              </div>
+              <div className={classes.about__text}>
+                <div className={classes.about__text__header}>Cześć! Jestem Klaudia i jestem trenerem personalnym w Katowicach</div>
+                <div className={classes.about__text__paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sunt nemo placeat, recusandae aliquid ea, aspernatur provident ullam maiores vel voluptate facilis iusto natus dolores laudantium minima? Mollitia, libero quaerat.</div>
+                <CustomButton text={'O mnie'} path={'/about'} color={'black'}/>
+              </div>
+        </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { Footer } from './components/Footer/Footer'
 import styles from './styles/_main.module.sass'
 import { Header } from './components/Header/Header'
+import { Welcome } from './components/Welcome/Welcome'
+import { Footer } from './components/Footer/Footer'
 import { About } from './components/About/About'
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header/>
+      <Welcome/>
       <About/>
       <Footer/>        
     </main>
