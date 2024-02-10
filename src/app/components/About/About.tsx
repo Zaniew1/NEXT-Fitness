b87fe83d7e217420476fc9142cf91cbaef79775c
Mateusz <m.zaniewski1995@gmail.com>
2024-02-10 20:50:58 +0100

    change styles about section

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -6,13 +6,12 @@ export const About:React.FC =():JSX.Element => {
     return(
         <section className={classes.about}>
           <div className={classes.about__header}>
-            <CustomHeader  text={"O mnie"}/>
           </div>
               <div className={classes.about__image}>
-                    <CustomImage src={'/klaudia.png'} alt={'Obraz przedstawiający Klaudię Schmidt'} priority={false} quality={90}/>
+                    <CustomImage src={'/klaudia.jpg'} alt={'Obraz przedstawiający Klaudię Schmidt'} priority={false} quality={90}/>
               </div>
               <div className={classes.about__text}>
-                <h3 className={classes.about__text__header}>Cześć!  Jestem Klaudia</h3>
+                <h3 className={classes.about__text__header}>Cześć! Jestem Klaudia</h3>
                 <CustomParagraph customClass={classes.about__text__paragraph} text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, maxime neque.isi aliquid, labore ratione, temporibus nam consequuntur repellendus repellat voluptas obcaecati illo iste at! o maxime asperiores reiciendis obcaecati, ab esse unde tempora et sint mollitia voluptate rem modi aperiam labore eius accusamus dolore at voluptatum cumque reprehenderit debitis qui ut fuga architecto? Quam."}/>
               </div>
         </section>
