34cd6f4adccf1cb57f67432f859e9edf5a9a652f
Mateusz <m.zaniewski1995@gmail.com>
2024-02-10 20:52:12 +0100

    change arrow section styles

--- src/app/components/UI/Buttons/NextArrow.tsx
+++ src/app/components/UI/Buttons/NextArrow.tsx
@@ -1,9 +1,9 @@
-import classes from "../../../styles/_opinions.module.sass";
+import classes from "../../../styles/_arrow.module.sass";
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faCaretRight} from "@fortawesome/free-solid-svg-icons";
 export const NextArrow:React.FC<{onClick:()=>void}> = (props):JSX.Element => {
     return (
-        <div onClick={props.onClick} className={`${classes.opinions__slider__overlapper__slick__arrow} ${classes.opinions__slider__overlapper__slick__arrow__right}`}>
+        <div onClick={props.onClick} className={`${classes.arrow} ${classes.arrow__right}`}>
             <FontAwesomeIcon icon={faCaretRight} />
         </div>
     )
--- src/app/components/UI/Buttons/PrevArrow.tsx
+++ src/app/components/UI/Buttons/PrevArrow.tsx
@@ -1,9 +1,9 @@
-import classes from "../../../styles/_opinions.module.sass";
+import classes from "../../../styles/_arrow.module.sass";
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 export const PrevArrow:React.FC<{onClick:()=>void}> = (props):JSX.Element => {
     return (
-        <div onClick={props.onClick} className={`${classes.opinions__slider__overlapper__slick__arrow} ${classes.opinions__slider__overlapper__slick__arrow__left}`}>
+        <div onClick={props.onClick} className={`${classes.arrow} ${classes.arrow__left}`}>
             <FontAwesomeIcon icon={faCaretLeft} />
         </div>
     )
