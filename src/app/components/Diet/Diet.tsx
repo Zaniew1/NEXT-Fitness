12d4015453f96a091b8ffe8bbd80787e60d409f1
Mateusz <m.zaniewski1995@gmail.com>
2024-02-10 20:49:03 +0100

    create diet page

---
+++ src/app/components/Diet/Diet.tsx
@@ -0,0 +1,8 @@
+import classes from '.././../styles/_diet.module.sass'
+export const Diet = ():JSX.Element => {
+    return (
+        <div className={classes.diet} >
+            <p className={classes.diet__paragraph}>Nie wiem co tutaj napisać </p>
+        </div>
+    );
+}
